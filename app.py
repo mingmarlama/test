76fed4a21272b738fbe476603a3cfeaa827b0dcd
mingmarlama <mingmar18lama@gmail.com>
2026-05-12 12:25:38 +0545

    First Proper Template

--- app.py
+++ app.py
@@ -8,3 +8,22 @@ def home():
 
 if __name__ ++ "__main__":
     app.run(debug=True)
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Welcome to the Home Page"
+
+@app.route("/about")
+def about():
+    return "This is the About Page"
+
+@app.route("/contact")
+def contact():
+    return "Contact us at [email protected]"
+
+if __name__ == "__main__":
+    app.run(debug=True)
